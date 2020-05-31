709efe6dcf9a7aa5a825d04c1a3a3de5d7cb0827
ViniEspinosaDev <viniespinosa,developer@gmail.com>
2020-05-31 16:01:16 -0300

    Criando cliente service

--- src/main/java/com/example/barbeariaviniespinosasamuelferraz/service/ClienteService.java
+++ src/main/java/com/example/barbeariaviniespinosasamuelferraz/service/ClienteService.java
@@ -1,8 +1,37 @@
 package com.example.barbeariaviniespinosasamuelferraz.service;
 
+import java.util.List;
+
+import com.example.barbeariaviniespinosasamuelferraz.entity.Cliente;
+import com.example.barbeariaviniespinosasamuelferraz.repository.ClienteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
-    
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> getClientes() {
+        return clienteRepository.findAll();
+    }
+
+    public void salvarCliente(Cliente cliente) {
+        clienteRepository.save(cliente);
+    }
+
+    public Cliente getClienteById(int id) {
+        return clienteRepository.findById(id).get();
+    }
+
+    public boolean removerCliente(Cliente cliente) {
+        // Talvez fazer o tratamento aqui futuramente
+        if (cliente.getSaloes().isEmpty() && cliente.getAgendamentos().isEmpty()) {
+            clienteRepository.delete(cliente);
+            return true;
+        }
+        return false;
+    }
 }
