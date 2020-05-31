0f000c6d4b82efe33bed4c212428edf8764b95ab
ViniEspinosaDev <viniespinosa,developer@gmail.com>
2020-05-31 15:43:07 -0300

    Criando agendamento repository

---
+++ src/main/java/com/example/barbeariaviniespinosasamuelferraz/repository/AgendamentoRepository.java
@@ -0,0 +1,9 @@
+package com.example.barbeariaviniespinosasamuelferraz.repository;
+
+import com.example.barbeariaviniespinosasamuelferraz.entity.Agendamento;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Integer> {
+
+}
