2f1df3b19b230ff93d42950fa2528de1e3ef7a9f
ViniEspinosaDev <viniespinosa,developer@gmail.com>
2020-05-31 15:44:42 -0300

    Criando salao repository

---
+++ src/main/java/com/example/barbeariaviniespinosasamuelferraz/repository/SalaoRepository.java
@@ -0,0 +1,9 @@
+package com.example.barbeariaviniespinosasamuelferraz.repository;
+
+import com.example.barbeariaviniespinosasamuelferraz.entity.Salao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalaoRepository extends JpaRepository<Salao, Integer> {
+
+}
