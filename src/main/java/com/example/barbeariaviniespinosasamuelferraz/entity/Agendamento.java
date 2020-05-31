694606c943635070cf9de49aad11fc9640ea8b8e
ViniEspinosaDev <viniespinosa,developer@gmail.com>
2020-05-31 15:29:43 -0300

    Implementando entidade agendamento

--- src/main/java/com/example/barbeariaviniespinosasamuelferraz/entity/Agendamento.java
+++ src/main/java/com/example/barbeariaviniespinosasamuelferraz/entity/Agendamento.java
@@ -1,5 +1,94 @@
 package com.example.barbeariaviniespinosasamuelferraz.entity;
 
-public class Agendamento {
-    
+import java.io.Serializable;
+import java.sql.Date;
+import java.sql.Time;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Agendamento implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int idAgendamento;
+
+    private Date data;
+    private Time hora;
+
+    @ManyToOne
+    @JoinColumn(name = "idSalao")
+    private Salao salao;
+
+    @ManyToOne
+    @JoinColumn(name = "idBarbeiro")
+    private Barbeiro barbeiro;
+
+    @ManyToOne
+    @JoinColumn(name = "idCliente")
+    private Cliente cliente;
+
+    public int getIdAgendamento() {
+        return idAgendamento;
+    }
+
+    public void setIdAgendamento(int idAgendamento) {
+        this.idAgendamento = idAgendamento;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Time getHora() {
+        return hora;
+    }
+
+    public void setHora(Time hora) {
+        this.hora = hora;
+    }
+
+    public Salao getSalao() {
+        return salao;
+    }
+
+    public void setSalao(Salao salao) {
+        this.salao = salao;
+    }
+
+    public Barbeiro getBarbeiro() {
+        return barbeiro;
+    }
+
+    public void setBarbeiro(Barbeiro barbeiro) {
+        this.barbeiro = barbeiro;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Agendamento [barbeiro=" + barbeiro + ", cliente=" + cliente + ", data=" + data + ", hora=" + hora
+                + ", idAgendamento=" + idAgendamento + ", salao=" + salao + "]";
+    }
+
 }
