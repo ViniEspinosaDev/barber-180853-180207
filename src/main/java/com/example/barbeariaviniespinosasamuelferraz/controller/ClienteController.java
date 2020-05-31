433393a00d2646f83c075db303426e45755437b6
ViniEspinosaDev <viniespinosa,developer@gmail.com>
2020-05-31 16:53:32 -0300

    Cadastro cliente feito

--- src/main/java/com/example/barbeariaviniespinosasamuelferraz/controller/ClienteController.java
+++ src/main/java/com/example/barbeariaviniespinosasamuelferraz/controller/ClienteController.java
@@ -1,8 +1,37 @@
 package com.example.barbeariaviniespinosasamuelferraz.controller;
 
+import com.example.barbeariaviniespinosasamuelferraz.entity.Cliente;
+import com.example.barbeariaviniespinosasamuelferraz.service.ClienteService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ClienteController {
-    
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping("/clientes")
+    public ModelAndView getClientes() {
+        ModelAndView mv = new ModelAndView("clientesTemplate");
+
+        mv.addObject("cliente", new Cliente());
+        mv.addObject("clientes", clienteService.getClientes());
+
+        return mv;
+    }
+
+    @PostMapping("/salvarCliente")
+    public String salvarCliente(@ModelAttribute Cliente cliente) {
+
+        clienteService.salvarCliente(cliente);
+
+        return "redirect:/clientes";
+    }
+
 }
