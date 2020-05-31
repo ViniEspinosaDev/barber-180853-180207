27dd92a2758bea84f1a6d99191f932da087e3b72
ViniEspinosaDev <viniespinosa,developer@gmail.com>
2020-05-31 15:46:13 -0300

    Criando barbeiro repository

---
+++ src/main/java/com/example/barbeariaviniespinosasamuelferraz/repository/BarbeiroRepository.java
@@ -0,0 +1,9 @@
+package com.example.barbeariaviniespinosasamuelferraz.repository;
+
+import com.example.barbeariaviniespinosasamuelferraz.entity.Barbeiro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BarbeiroRepository extends JpaRepository<Barbeiro, Integer> {
+
+}
